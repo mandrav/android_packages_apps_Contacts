2a4146fe758f5ff692f254976b31c4ca553ffe74
Danesh M <daneshm90@gmail.com>
2011-12-03 19:51:25 -0500

    Added sort mode options in preferences Introduced matchId to keep track of preference in the results. Introduced allResults as a set to eliminate duplications.
    Change-Id: I705391c7367dd0b4c4725f36a57b2f1ec8ae2e15

--- src/com/android/contacts/dialpad/T9Search.java
+++ src/com/android/contacts/dialpad/T9Search.java
@@ -6,11 +6,18 @@ package com.android.contacts.dialpad;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.Contacts;
 import android.provider.MediaStore.Images.Media;
@@ -32,8 +39,11 @@ class T9Search {
     private static final String PHONE_ID_QUERY = Phone.CONTACT_ID + " = ?";
     private static final String PHONE_QUERY = Phone.NORMALIZED_NUMBER + " GLOB ? OR " + Phone.NUMBER + " GLOB ?";
     private static final String PHONE_QUERY_SORT = Phone.IS_SUPER_PRIMARY + " desc";
-
+    private static final int NAME_FIRST = 1;
+    private static final int NUMBER_FIRST = 2;
+    private static final int DIRECT_NUMBER = 3;
     private Context mContext;
+    private int mSortMode;
 
     public T9Search(Context context) {
         mContext = context;
@@ -71,12 +81,16 @@ class T9Search {
         Bitmap photoUri;
         String name;
         String number;
+        int matchId;
     }
 
     public T9SearchResult search(String number) {
         number=number.replaceAll( "[^\\d]", "" );
         T9SearchResult result = null;
-        ArrayList<ContactItem> allResults = new ArrayList<ContactItem>();
+        ArrayList<ContactItem> nameResults = new ArrayList<ContactItem>();
+        ArrayList<ContactItem> numberResults = new ArrayList<ContactItem>();
+        Set<ContactItem> allResults = new LinkedHashSet<ContactItem>();
+        mSortMode = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(mContext).getString("t9_sort", "1"));
 
         Cursor c = searchPhones(number);
         try {
@@ -84,9 +98,10 @@ class T9Search {
                 ContactItem temp = new ContactItem();
                 temp.name = c.getString(1);
                 temp.number = c.getString(2);
+                temp.matchId = temp.number.indexOf(number);
                 if (c.getString(4)!=null)
-                temp.photoUri = Media.getBitmap(mContext.getContentResolver(), Uri.parse(c.getString(4)));
-                allResults.add(temp);
+                    temp.photoUri = Media.getBitmap(mContext.getContentResolver(), Uri.parse(c.getString(4)));
+                numberResults.add(temp);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -102,11 +117,10 @@ class T9Search {
                 ContactItem temp = new ContactItem();
                 temp.name = c.getString(1);
                 temp.number = getBestPhone(c.getString(0));
+                temp.matchId = getNameMatchId(temp.name,number);
                 if (c.getString(3)!=null)
-                temp.photoUri = Media.getBitmap(mContext.getContentResolver(), Uri.parse(c.getString(3)));
-                if (!allResults.contains(temp)){
-                    allResults.add(temp);
-                }
+                    temp.photoUri = Media.getBitmap(mContext.getContentResolver(), Uri.parse(c.getString(3)));
+                nameResults.add(temp);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -115,11 +129,39 @@ class T9Search {
         } finally {
             c.close();
         }
-
-        if (allResults.size() > 0) {
-            result = new T9SearchResult(allResults);
+        Collections.sort(nameResults, new CustomComparator());
+        Collections.sort(numberResults, new CustomComparator());
+        if (nameResults.size() > 0 || numberResults.size() > 0) {
+            switch (mSortMode) {
+            case NAME_FIRST:
+                allResults.addAll(nameResults);
+                allResults.addAll(numberResults);
+                break;
+            case NUMBER_FIRST:
+            case DIRECT_NUMBER:
+                allResults.addAll(numberResults);
+                allResults.addAll(nameResults);
+            }
+            return new T9SearchResult(new ArrayList<ContactItem>(allResults));
         }
         return result;
+    }
+
+    public class CustomComparator implements Comparator<ContactItem> {
+        @Override
+        public int compare(ContactItem lhs, ContactItem rhs) {
+            return Integer.compare(lhs.matchId,rhs.matchId);
+        }
+    }
+
+    private int getNameMatchId(String name, String input) {
+        Pattern pattern = Pattern.compile(buildT9ContactQuery(input),Pattern.CASE_INSENSITIVE);
+        Matcher m = pattern.matcher(name);
+        if (m.find()){
+            return m.start();
+        }else{
+            return name.length();
+        }
     }
 
     private String getBestPhone(String contactId) {
@@ -140,11 +182,18 @@ class T9Search {
     }
 
     private Cursor searchPhones(String number) {
-        return mContext.getContentResolver().query(Phone.CONTENT_URI,
-                PHONE_PROJECTION,
-                PHONE_QUERY,
-                new String[] {"*" + number + "*","*" + number + "*"},
-                PHONE_QUERY_SORT);
+        String query = null;
+        if (mSortMode==DIRECT_NUMBER){
+            Uri contactUri = Uri.withAppendedPath(Phone.CONTENT_FILTER_URI, Uri.encode(number));
+            return mContext.getContentResolver().query(contactUri, PHONE_PROJECTION, null, null, PHONE_QUERY_SORT);
+        }else{
+            query = "*" + number + "*";
+            return mContext.getContentResolver().query(Phone.CONTENT_URI,
+                    PHONE_PROJECTION,
+                    PHONE_QUERY,
+                    new String[] {query,query},
+                    PHONE_QUERY_SORT);
+        }
     }
 
     private Cursor searchContacts(String number) {
